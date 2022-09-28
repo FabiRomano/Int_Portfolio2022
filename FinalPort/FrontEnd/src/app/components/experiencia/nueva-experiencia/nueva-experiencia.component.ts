1bc6f1430d9b005cb5cc20e60de324dca01f6195
FabiRomano <99614961+FabiRomano@users.noreply.github.com>
2022-09-28 19:14:45 -0300

    nuevaEducacion

--- FinalPort/FrontEnd/src/app/components/experiencia/nueva-experiencia/nueva-experiencia.component.ts
+++ FinalPort/FrontEnd/src/app/components/experiencia/nueva-experiencia/nueva-experiencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Experience } from 'src/app/models/experience';
+import { ExperienceService } from 'src/app/servicios/experience.service';
 
 @Component({
   selector: 'app-nueva-experiencia',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevaExperienciaComponent implements OnInit {
 
-  constructor() { }
+  empresaExp: string = '';
+  fechaInicioExp:string = '';
+  fechaFinExp: string = '';
+  descripcionExp: string = '';
+
+  constructor(
+    private experienceService : ExperienceService,
+    private router : Router
+    ) { }
 
   ngOnInit(): void {
   }
 
+  onAgregarExperience(): void {
+    const experiences = new Experience(
+      this.empresaExp,
+      this.fechaInicioExp,
+      this.fechaFinExp,
+      this.descripcionExp,
+     
+    );
+    this.experienceService.agregarExperiece(experiences).subscribe(
+      (data) => {
+        alert('Nueva Experiencia añadida');
+        this.router.navigate(['/experiencia']);
+
+        // console.log(JSON.stringify(experiences));
+      },
+    
+      (err) => {
+        alert('la operacion fallo');
+        this.router.navigate(['']);
+      }
+    );
+  }
 }
